f35ce337a7010c7f056bff308ad140e03a8e134a
Chris Mason <57402469+cmason3@users.noreply.github.com>
2020-06-01 08:04:46 +0100

    Update jinjafx_server.py

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -315,7 +315,6 @@ def log(t):
 
 def w_directory(d):
   if not os.path.isdir(d):
-    print("Checking for " + str(d))
     raise argparse.ArgumentTypeError("repository directory must exist")
   elif not os.access(d, os.W_OK):
     raise argparse.ArgumentTypeError("repository directory must be writable")
