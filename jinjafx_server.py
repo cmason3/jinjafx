6109f6f3123301160d0b38a083f261d2ac5a4bd5
Chris Mason <57402469+cmason3@users.noreply.github.com>
2020-06-01 08:10:39 +0100

    Update jinjafx_server.py

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -314,6 +314,7 @@ def log(t):
 
 
 def w_directory(d):
+  print("Checking: " + str(d))
   if not os.path.isdir(d):
     raise argparse.ArgumentTypeError("repository directory must exist")
   elif not os.access(d, os.W_OK):
