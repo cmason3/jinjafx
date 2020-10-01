69e6fa5d315b5f3b975882a97b95daa8a2f0a032
Chris Mason <57402469+cmason3@users.noreply.github.com>
2020-10-01 20:43:12 +0100

    Update jinjafx.js

--- www/jinjafx.js
+++ www/jinjafx.js
@@ -202,7 +202,7 @@ window.onload = function() {
       cursor: "row-resize",
       sizes: [30, 70],
       snapOffset: 0,
-      minSize: 25 
+      minSize: 30
     });
 
     document.getElementById('ldata').onclick = function() {
