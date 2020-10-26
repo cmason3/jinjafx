5d94a429fea32450535c531ed160f04d1466e516
Chris Mason <57402469+cmason3@users.noreply.github.com>
2020-10-26 18:48:40 +0000

    version bump to 1.2.1

--- jinjafx.py
+++ jinjafx.py
@@ -18,7 +18,7 @@
 from __future__ import print_function, division
 import sys, os, jinja2, yaml, argparse, re, copy, traceback
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 jinja2_filters = []
 
 class ArgumentParser(argparse.ArgumentParser):
