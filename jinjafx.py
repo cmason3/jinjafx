de1f8639793e68de2c3d556480a9d43f4fbc5fc3
Chris Mason <57402469+cmason3@users.noreply.github.com>
2020-12-01 17:13:15 +0000

    remove beta

--- jinjafx.py
+++ jinjafx.py
@@ -18,7 +18,7 @@
 from __future__ import print_function, division
 import sys, os, jinja2, yaml, argparse, re, copy, traceback
 
-__version__ = '1.2.3 (beta)'
+__version__ = '1.2.3'
 jinja2_filters = []
 
 class ArgumentParser(argparse.ArgumentParser):
