1ebd3b8c08804c238c7b62fc56e6ee255d14f191
Chris Mason <57402469+cmason3@users.noreply.github.com>
2021-02-19 19:30:05 +0000

    add beta flag

--- jinjafx.py
+++ jinjafx.py
@@ -18,7 +18,7 @@
 from __future__ import print_function, division
 import sys, os, socket, jinja2, yaml, argparse, re, copy, traceback
 
-__version__ = '1.3.3'
+__version__ = '1.3.4 (beta)'
 jinja2_filters = []
 
 class ArgumentParser(argparse.ArgumentParser):
