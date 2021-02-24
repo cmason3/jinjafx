a927978f65d17e9883f6912e81d928da8958444d
Chris Mason <57402469+cmason3@users.noreply.github.com>
2021-02-24 08:31:58 +0000

    add beta flag and bump version to v1.4.0

--- jinjafx.py
+++ jinjafx.py
@@ -18,7 +18,7 @@
 from __future__ import print_function, division
 import sys, os, socket, jinja2, yaml, argparse, re, copy, traceback
 
-__version__ = '1.3.4'
+__version__ = '1.4.0 (beta)'
 jinja2_filters = []
 
 class ArgumentParser(argparse.ArgumentParser):
