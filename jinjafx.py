3cf47cf1bd7f61f3a766e4a09427d2b47c9f0dea
Chris Mason <57402469+cmason3@users.noreply.github.com>
2021-03-24 20:43:45 +0000

    Update jinjafx.py

--- jinjafx.py
+++ jinjafx.py
@@ -18,7 +18,7 @@
 from __future__ import print_function, division
 import sys, os, socket, jinja2, yaml, argparse, re, copy, traceback
 
-__version__ = '1.4.1 (beta)'
+__version__ = '1.5.0 (beta)'
 jinja2_filters = []
 
 class ArgumentParser(argparse.ArgumentParser):
