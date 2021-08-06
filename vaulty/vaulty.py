a45c163bc856b47c1a708c74efe31492797eeb50
Chris Mason <57402469+cmason3@users.noreply.github.com>
2021-08-06 07:21:56 +0100

    Update vaulty.py

--- vaulty/vaulty.py
+++ vaulty/vaulty.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+# Vaulty - Encrypt/Decrypt with ChaCha20-Poly1305
+# Copyright (c) 2021 Chris Mason <chris@netnix.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 import sys, os, base64, getpass
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
@@ -70,21 +85,21 @@ def args():
       if len(sys.argv) == 2 or os.path.isfile(sys.argv[2]):
         return m
 
-def main(m=args()):
+def main(m=args(), cols=80):
   if m is not None:
     if len(sys.argv) == 2:
       data = sys.stdin.read().encode('utf-8')
 
-    password = getpass.getpass('Enter Password: ').encode('utf-8')
+    password = getpass.getpass('enter password: ').encode('utf-8')
 
     if m == 'encrypt':
-      if password == getpass.getpass('Verify Password: ').encode('utf-8'):
+      if password == getpass.getpass('verify password: ').encode('utf-8'):
         if len(sys.argv) == 2:
-          ciphertext = Vaulty().encrypt(data, password, 80).decode('utf-8')
+          ciphertext = Vaulty().encrypt(data, password, cols).decode('utf-8')
           print(ciphertext, end='')
     
         else:
-          b = Vaulty().encrypt_file(sys.argv[2], password, 80)
+          b = Vaulty().encrypt_file(sys.argv[2], password, cols)
 
       else:
         print('error: password verification failed', file=sys.stderr)
@@ -96,15 +111,15 @@ def main(m=args()):
           print(plaintext.decode('utf-8'), end='')
 
         else:
-          print('error: invalid password or not encrypted', file=sys.stderr)
+          print('error: invalid password or data not encrypted', file=sys.stderr)
 
       else:
         b = Vaulty().decrypt_file(sys.argv[2], password)
         if b is None:
-          print('error: invalid password or not encrypted', file=sys.stderr)
+          print('error: invalid password or file not encrypted', file=sys.stderr)
 
   else:
-    print('usage: vaulty encrypt|decrypt [filepath]', file=sys.stderr)
+    print('usage: vaulty encrypt|decrypt [file]', file=sys.stderr)
 
 
 if __name__ == '__main__':
