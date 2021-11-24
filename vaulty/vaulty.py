ea375222ebb908b30b6d22500333afa32aadf5df
Chris Mason <chris@netnix.org>
2021-11-24 10:32:26 +0000

    update vaulty.py

--- vaulty/vaulty.py
+++ vaulty/vaulty.py
@@ -25,48 +25,54 @@ class Vaulty():
   def __init__(self):
     self.__version__ = '1.0.0'
     self.__prefix = '$VAULTY;'
-    self.__extension = '.vy'
+    self.__extension = '.vlt'
     self.__kcache = {}
 
   def __derive_key(self, password, salt=None):
-    if salt is None:
-      salt = os.urandom(16)
-      
     ckey = (password, salt)
-  
+
     if ckey in self.__kcache:
       return self.__kcache[ckey]
+
+    if salt is None:
+      salt = os.urandom(16)
   
     key = Scrypt(salt, 32, 2**16, 8, 1, default_backend()).derive(password)
     self.__kcache[ckey] = [salt, key]
     return salt, key
   
-  def encrypt(self, plaintext, password, cols=None):
+  def encrypt(self, plaintext, password, cols=None, armour=True):
+    version = b'\x01'
     salt, key = self.__derive_key(password)
     nonce = os.urandom(12)
     ciphertext = ChaCha20Poly1305(key).encrypt(nonce, plaintext, None)
-    r = self.__prefix.encode('utf-8') + base64.b64encode(b'\x01' + salt + nonce + ciphertext)
 
-    if cols is not None:
-      r = b'\n'.join([r[i:i + cols] for i in range(0, len(r), cols)])
+    if armour:
+      r = self.__prefix.encode('utf-8') + base64.b64encode(version + salt + nonce + ciphertext)
 
-    return r + b'\n'
+      if cols is not None:
+        r = b'\n'.join([r[i:i + cols] for i in range(0, len(r), cols)])
+
+      return r + b'\n'
+
+    else:
+      return version + salt + nonce + ciphertext
   
   def decrypt(self, ciphertext, password):
     try:
-      ciphertext = ciphertext.strip()
-      if ciphertext.startswith(self.__prefix.encode('utf-8')):
-        ciphertext = base64.b64decode(ciphertext[8:])
-        if ciphertext.startswith(b'\x01') and len(ciphertext) > 29:
-          key = self.__derive_key(password, ciphertext[1:17])[1]
-          return ChaCha20Poly1305(key).decrypt(ciphertext[17:29], ciphertext[29:], None)
+      if ciphertext.lstrip().startswith(self.__prefix.encode('utf-8')):
+        ciphertext = base64.b64decode(ciphertext.strip()[8:])
+
+      if len(ciphertext) > 29 and ciphertext.startswith(b'\x01'):
+        key = self.__derive_key(password, ciphertext[1:17])[1]
+        return ChaCha20Poly1305(key).decrypt(ciphertext[17:29], ciphertext[29:], None)
 
     except InvalidTag:
       pass
 
   def encrypt_file(self, filepath, password, cols=None):
     with open(filepath, 'rb') as fh:
-      ciphertext = self.encrypt(fh.read(), password, cols)
+      ciphertext = self.encrypt(fh.read(), password, cols, False)
 
     if ciphertext is not None:
       with open(filepath, 'wb') as fh:
@@ -101,7 +107,7 @@ def args():
 def main(m=args(), cols=80, v = Vaulty()):
   if m is not None:
     if len(sys.argv) == 2:
-      data = sys.stdin.read().encode('utf-8')
+      data = sys.stdin.buffer.read()
 
     password = getpass.getpass('Vaulty Password: ').encode('utf-8')
     if len(password) > 0:
