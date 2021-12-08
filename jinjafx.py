ce0d55f55765dd52caff2f0d597b72a18f8280d5
Chris Mason <chris.mason@telefonica.com>
2021-12-08 08:34:21 +0000

    bump version and copyright year

--- jinjafx.py
+++ jinjafx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # JinjaFx - Jinja2 Templating Tool
-# Copyright (c) 2020-2021 Chris Mason <chris@netnix.org>
+# Copyright (c) 2020-2022 Chris Mason <chris@netnix.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -17,7 +17,7 @@
 
 import sys, os, jinja2, yaml, argparse, re, copy, getpass, traceback
 
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 jinja2_filters = []
 
 def import_filters(errc = 0):
@@ -65,7 +65,7 @@ def main():
   try:
     if '-q' not in sys.argv:
       print('JinjaFx v' + __version__ + ' - Jinja2 Templating Tool')
-      print('Copyright (c) 2020-2021 Chris Mason <chris@netnix.org>\n')
+      print('Copyright (c) 2020-2022 Chris Mason <chris@netnix.org>\n')
 
     jinjafx_usage = '(-t <template.j2> [-d <data.csv>] | -dt <dt.yml>) [-g <vars.yml>] [-o <output file>] [-od <output dir>] [-m] [-q]'
 
