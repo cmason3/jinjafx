244cddaa7c32de2b39d32b237e4f00aeb94327d3
Chris Mason <chris.mason@virginmediao2.co.uk>
2022-02-15 11:05:40 +0000

    add junos6hash

--- jinjafx.py
+++ jinjafx.py
@@ -18,7 +18,7 @@
 import sys, os, io, argparse, re, copy, getpass, datetime, traceback
 import jinja2, yaml, pytz
 
-__version__ = '1.9.2'
+__version__ = '1.9.3'
 jinja2_filters = []
 
 def import_filters(errc = 0):
