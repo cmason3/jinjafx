b04a83e2f8ac4204573358c92c2ca45f2a1f47e7
Chris Mason <57402469+cmason3@users.noreply.github.com>
2022-05-01 10:08:38 +0100

    Update jinjafx.py

--- jinjafx.py
+++ jinjafx.py
@@ -28,7 +28,7 @@ from cryptography.hazmat.primitives.ciphers.modes import CTR
 from cryptography.hazmat.backends import default_backend
 from cryptography.exceptions import InvalidSignature
 
-__version__ = '1.11.5'
+__version__ = '1.11.6'
 
 def main():
   try:
