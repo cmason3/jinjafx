3d0e53a85a10caed191b6982166e4522b33a4fc3
Chris Mason <chris.mason@virginmediao2.co.uk>
2023-02-10 00:04:41 +0000

    add mypy types

--- extensions/ext_jinjafx.py
+++ extensions/ext_jinjafx.py
@@ -101,17 +101,18 @@ class plugin(Extension):
     NUM_ALPHA = [char for char in ''.join(FAMILY)]
     ALPHA_NUM = { NUM_ALPHA[i]: i for i, c in enumerate(NUM_ALPHA) }
   
-    def random_salt(length):
-      salt = ''
+    def random_salt(length: int) -> str:
+      salt: str = ''
 
       for i in range(length):
         salt += NUM_ALPHA[random.randrange(len(NUM_ALPHA))]
 
       return salt
   
-    def gap_encode(char, prev, encode):
-      gaps = []
+    def gap_encode(char: str, prev: str, encode: list[int]) -> str:
+      gaps: list[int] = []
       val = ord(char)
+
       for e in encode[::-1]:
         gaps.insert(0, val // e)
         val %= e
@@ -267,7 +268,7 @@ class plugin(Extension):
 
     return self.__sha512_crypt(string, salt)
 
-  def __xpath(self, s_xml, s_path):
+  def __xpath(self, s_xml: str, s_path: str) -> list[str]:
     if lxml:
       s_xml = re.sub(r'>\s+<', '><', s_xml.strip())
       xml = etree.fromstring(s_xml, parser=etree.XMLParser(remove_comments=True, remove_pis=True))
