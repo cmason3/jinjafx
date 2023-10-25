b6d6a13102813928840c87581516b8246351ad5a
Chris Mason <chris.mason@virginmediao2.co.uk>
2023-10-25 10:32:39 +0100

    add support for hierarchical counters

--- jinjafx.py
+++ jinjafx.py
@@ -31,7 +31,7 @@ from cryptography.hazmat.primitives.ciphers.algorithms import AES
 from cryptography.hazmat.primitives.ciphers.modes import CTR
 from cryptography.exceptions import InvalidSignature
 
-__version__ = '1.18.1'
+__version__ = '1.18.1-dev'
 
 __all__ = ['JinjaFx', 'Vault']
 
