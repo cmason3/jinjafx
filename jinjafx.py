41bf67d6fb947a2c821ea486b2ed6ba5a7b65d57
Chris Mason <chris@netnix.org>
2024-05-10 08:53:57 +0100

    update

--- jinjafx.py
+++ jinjafx.py
@@ -31,7 +31,7 @@ from cryptography.hazmat.primitives.ciphers.algorithms import AES
 from cryptography.hazmat.primitives.ciphers.modes import CTR
 from cryptography.exceptions import InvalidSignature
 
-__version__ = '1.20.0'
+__version__ = '1.20.1'
 
 __all__ = ['JinjaFx', 'Vault']
 
